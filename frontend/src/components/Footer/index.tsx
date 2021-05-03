0ebc5cdd325c4069884b45e3256c80d67ac5b8fb
Felipe Lobo <felipeblobo@gmail.com>
2021-05-03 16:14:33 -0300

    ajustes no layout

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -1,22 +1,21 @@
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 const Footer = () => {
   return (
     <footer className="footer mt-auto py-3 bg-dark">
       <div className="container">
-        <p className="text-light">
-          App desenvolvido por{" "}
-          <a
-            href="https://github.com/felipeblobo"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Felipe Lôbo
-          </a>
-        </p>
+        <div className='footerItem footerMargin text-light'>
+          <small>Desenvolvido por:</small>
+        <a target='_blank' rel="noreferrer" href='https://github.com/felipeblobo'><FontAwesomeIcon className = 'icone mx-2' icon={faGithub} size="2x" style={{color: '#ffff'}} /></a>
+        <a target='_blank' rel="noreferrer" href='https://www.linkedin.com/in/felipelobo23'><FontAwesomeIcon className = 'icone' icon={faLinkedin} size="2x" style={{color: '#ffff'}} /></a>
+        </div>
+        
         <p className="text-light">
           <small>
-            <strong>Semana Spring React</strong>
-            <br />
-            Evento promovido pela escola DevSuperior:{" "}
+            <strong>Semana Spring React </strong>
+            - Evento promovido pela escola DevSuperior:{" "}
             <a
               href="https://instagram.com/devsuperior.ig"
               target="_blank"
--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,12 +1,13 @@
 import logo from "../../assets/images/logo.png";
-
+import logoIcon from "../../assets/images/dashboard_02.svg";
 
 const Navbar = () => {
   return (
     <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
       <div className="container">
         <nav className="my-2 my-md-0 mr-md-3">
-          <img src={logo} alt="DevSuperior" width="120" />
+          <img src={logoIcon} alt="logo" />
+          <img src={logo} alt="devdash" width="200" />
         </nav>
       </div>
     </div>
