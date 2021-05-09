4f7e5ecacb843665e3d7de4f68523cf0cf870704
Felipe Lobo <felipeblobo@gmail.com>
2021-05-08 23:28:52 -0300

    data table integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,10 +1,26 @@
+import {Seller} from './seller';
+
+export type Sale = {
+  id: number;
+  visited: number;
+  deals: number;
+  amount: number;
+  date: string;
+  seller: Seller;
+}
+
+
+export type SalePage = {
+  
+}
+
+
 export type SaleSum = {
-  sellerName: string,
-  sum: number
+  sellerName: string;
 }
 
 export type SaleSuccess = {
-  sellerName: string,
-  visited: number,
-  deals: number
+  sellerName: string;
+  visited: number;
+  deals: number;
 }
