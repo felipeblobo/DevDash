01489b93df994e0e39c895d36914d156c992cff9
Felipe Lobo <felipeblobo@gmail.com>
2021-05-09 01:30:27 -0300

    ajust in Home Page

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -7,12 +7,12 @@ const Home = () => {
     <>
       <Navbar />
       <div className="container">
-        <div className="jumbotron">
-            <h1 className="display-4">DSVendas</h1>
+        <div className="jumbotron my-4 bg-light ">
+            <h1 className="display-4">DevDash</h1>
             <p className="lead">Analise o desempenho das suas vendas por diferentes perspectivas</p>
             <hr/>
             <p>Esta aplicação consiste em exibir um dashboard a partir de dados fornecidos por um back end construído com Spring Boot.</p>
-            <Link className='btn btn-primary btn-lg' to='/dashboard'>
+            <Link id='dashButton' className=' btn bg-primary btn-lg' to='/dashboard'>
               Acessar Dashboard
             </Link>
         </div>
